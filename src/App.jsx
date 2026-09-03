1f5ff575542bcc693764cf10a7186ee359243b3c
Sabinaya Khadka <sabinayakhadka3@gmail.com>
2026-09-03 09:16:29 +0545

    Minor changes to project section

--- src/App.jsx
+++ src/App.jsx
@@ -69,22 +69,29 @@ function App() {
 
         <h2>THINGS I'M BUILDING</h2>
         <div className="project-card">
-          <p className="project-number">NO.1--IN PROGRESS</p>
-          <h3>PORTFOLIO MANAGEMENT SYSTEM</h3>
-          <p>
-            A web application I'm building to manage demat accounts, holdings, companies, and IPO news while learning web development.
-          </p>
-          <div className="project-tags">
-            <span>HTML</span>
-            <span>CSS</span>
-            <span>JAVASCRIPT</span>
-            <span>PHP</span>
-            <span>MYSQL</span>
-          </div>
-          <span className="project-status">BUILDING</span>
-        </div>
+  <div className="project-top">
+    <p className="project-number">NO. 01 — IN PROGRESS</p>
+  </div>
+  <h3>PORTFOLIO MANAGEMENT SYSTEM</h3>
+  <p className="project-description">
+    A web application I'm building to manage demat accounts, holdings,
+    companies, and IPO news while learning full-stack web development.
+  </p>
+  <div className="project-tags">
+    <span>HTML</span>
+    <span>CSS</span>
+    <span>JAVASCRIPT</span>
+    <span>PHP</span>
+    <span>MYSQL</span>
+  </div>
+    {/*<span className="project-status">BUILDING</span> Commented it out Just in case*/}
+    
+  <div className="project-bottom">
+   {/* <span>FULL-STACK WEB APP</span><br></br> */}
+    <a href="https://github.com/sabinaya-404/portfolio-management-system" className="project-status" target = "_blank" rel = "noopener noreferrer">VIEW ON GITHUB</a>
+  </div>
+</div>
       </section>
-
 
       {/*Learning Section*/}
       <section id="learning" className="content-section learning-section">
